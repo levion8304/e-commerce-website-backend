fc7b742c2974cad7298471b404bab0d8a6df574e
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 14:36:26 +0700

    Update index.route.js

--- api/v1/routes/seller/index.route.js
+++ api/v1/routes/seller/index.route.js
@@ -3,11 +3,9 @@ const orderRoutes = require("./order.route");
 const profileRoutes = require("./profile.route");
 
 const systemConfig = require("../../../../config/system");
-const { requireAuth } = require("../../middlewares/seller/auth.middleware");
 
 module.exports = (app) => {
   const version = "/api/v1";
-  app.use(requireAuth);
 
   app.use(version + systemConfig.prefixSeller + "/product", productRoutes);
   app.use(version + systemConfig.prefixSeller + "/order", orderRoutes);
