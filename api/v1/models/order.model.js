b316d7703e0f8c77a22879caaaa9e02806d6f940
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 15:17:11 +0700

    Update order.model.js

--- api/v1/models/order.model.js
+++ api/v1/models/order.model.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
-const { generateOrderId } = require("../../../helpers/generate");
 
 const orderSchema = new mongoose.Schema(
   {
     userId: String,
     orderId: {
       type: String,
-      default: generateOrderId(15),
     },
     status: Array,
     products: Array,
