401f5174ac2a5b9959c377b7d4fbea79a93f3b82
Ho Tran Ngoc Liem <ngocliem10a5nth@gmail.com>
2024-12-13 14:02:43 +0700

    Update index.route.js

--- api/v1/routes/client/index.route.js
+++ api/v1/routes/client/index.route.js
@@ -10,7 +10,15 @@ module.exports = (app) => {
   const version = "/api/v1";
 
   app.use(version + "/products", requireAuth, productRoutes);
-  app.use(version + "/user", userRoutes);
+  app.use(version + "/user", (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Origin');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  }, userRoutes);
   app.use(version + "/cart", requireAuth, cartRoutes);
   app.use(version + "/checkout", requireAuth, checkoutRoutes);
   app.use(version + "/category", requireAuth, categoryRoutes);
